4526e18051e4fc3d854e8e1e9e82c2ed5134bfcb
Kjartan Birgisson <kjartan@stubbur.com>
2026-01-18 16:24:04 +0000

    Ákvað að það væri óþarfi að hafa há stafi og blandað verum bara með lástafi.

--- Tools/TextAnalysis/Alphabets/Alphabets.cs
+++ Tools/TextAnalysis/Alphabets/Alphabets.cs
@@ -8,24 +8,12 @@ namespace Tools.TextAnalysis.Language
 {
     internal static class Alphabets
     {
-        static readonly string[] IcelandicAlphabetUpperCases = new string[]
-        {
-            "A","Á","B","D","Ð","E","É","F","G","H","I","Í","J","K","L","M",
-            "N","O","Ó","P","R","S","T","U","Ú","V","X","Y","Ý","Z","Þ","Æ","Ö"
-        };
-        static readonly string[] IcelandicAlphabetBothCases = new string[]
-        {
-            "A","Á","B","D","Ð","E","É","F","G","H","I","Í","J","K","L","M",
-            "N","O","Ó","P","R","S","T","U","Ú","V","X","Y","Ý","Z","Þ","Æ","Ö",
-            "a","á","b","d","ð","e","é","f","g","h","i","í","j","k","l","m",
-            "n","o","ó","p","r","s","t","u","ú","v","x","y","ý","z","þ","æ","ö"
-        };
-        static readonly string[] IcelandicAlphabetLowerCases = new string[]
+        static readonly string[] IcelandicAlphabet = new string[]
         {
             "a","á","b","d","ð","e","é","f","g","h","i","í","j","k","l","m",
             "n","o","ó","p","r","s","t","u","ú","v","x","y","ý","z","þ","æ","ö"
         };
-        static readonly string[] IcelandicAlphabetLowerCasesWithEnglishLetters = new string[]
+        static readonly string[] IcelandicAlphabetWithEnglishLetters = new string[]
         {
             "a","á","b","c","d","ð","e","é","f","g","h","i","í","j","k","l","m",
             "n","o","ó","p","q","r","s","t","u","ú","v","w","x","y","ý","z","þ","æ","ö"
