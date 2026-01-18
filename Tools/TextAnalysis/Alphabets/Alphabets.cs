d5fb144c76919c93fd14ed3539dce6b31dfe2e1c
Kjartan Birgisson <kjartan@stubbur.com>
2026-01-18 16:22:57 +0000

    Bætti inn Íslensku Stafasettinu ásamt ensku

---
+++ Tools/TextAnalysis/Alphabets/Alphabets.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Tools.TextAnalysis.Language
+{
+    internal static class Alphabets
+    {
+        static readonly string[] IcelandicAlphabetUpperCases = new string[]
+        {
+            "A","Á","B","D","Ð","E","É","F","G","H","I","Í","J","K","L","M",
+            "N","O","Ó","P","R","S","T","U","Ú","V","X","Y","Ý","Z","Þ","Æ","Ö"
+        };
+        static readonly string[] IcelandicAlphabetBothCases = new string[]
+        {
+            "A","Á","B","D","Ð","E","É","F","G","H","I","Í","J","K","L","M",
+            "N","O","Ó","P","R","S","T","U","Ú","V","X","Y","Ý","Z","Þ","Æ","Ö",
+            "a","á","b","d","ð","e","é","f","g","h","i","í","j","k","l","m",
+            "n","o","ó","p","r","s","t","u","ú","v","x","y","ý","z","þ","æ","ö"
+        };
+        static readonly string[] IcelandicAlphabetLowerCases = new string[]
+        {
+            "a","á","b","d","ð","e","é","f","g","h","i","í","j","k","l","m",
+            "n","o","ó","p","r","s","t","u","ú","v","x","y","ý","z","þ","æ","ö"
+        };
+        static readonly string[] IcelandicAlphabetLowerCasesWithEnglishLetters = new string[]
+        {
+            "a","á","b","c","d","ð","e","é","f","g","h","i","í","j","k","l","m",
+            "n","o","ó","p","q","r","s","t","u","ú","v","w","x","y","ý","z","þ","æ","ö"
+        };
+    }
+}
---
+++ Tools/TextAnalysis/FrequencyCounter.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Tools.TextAnalysis
+{
+    public class FrequencyCounter
+    {
+        public Dictionary<string,int> CountFrequencies(string text)
+        {
+            var frequencies = new Dictionary<string, int>(StringComparer.OrdinalIgnoreCase);
+            var words = text.Split(new[] { ' ', '\n', '\r', '\t', '.', ',', ';', '!', '?' }, StringSplitOptions.RemoveEmptyEntries);
+            foreach (var word in words)
+            {
+                if (frequencies.ContainsKey(word))
+                {
+                    frequencies[word]++;
+                }
+                else
+                {
+                    frequencies[word] = 1;
+                }
+            }
+            return frequencies;
+        }
+        static public Dictionary<char,int> CountCharacterFrequencies(string text)
+        {
+            var frequencies = new Dictionary<char, int>();
+            foreach (var ch in text)
+            {
+                if (char.IsWhiteSpace(ch)) continue;
+                if (frequencies.ContainsKey(ch))
+                {
+                    frequencies[ch]++;
+                }
+                else
+                {
+                    frequencies[ch] = 1;
+                }
+            }
+            return frequencies;
+        }
+    }
+}
