1a89911a3a4518984e123f4976bd710f179a6121
Kjartan Birgisson <kjartan@stubbur.com>
2026-01-18 14:24:23 +0000

    Add project files.

---
+++ Tools/CryptoGraphy/Substition/Ceasar.cs
@@ -0,0 +1,15 @@
+﻿namespace Tools.CryptoGraphy.Substition
+{
+    public class Ceasar
+    {
+        string Encrypt(string Text)
+        {
+            return Text;
+        }
+
+        string Decrypt(string Text)
+        {
+            return Text;
+        }
+    }
+}
