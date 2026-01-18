6d114c6413a3bc2a9cb4e39d04e5a022acee57b1
Kjartan Birgisson <kjartan@stubbur.com>
2026-01-18 16:22:09 +0000

    Bjó til grunninn og er að hugsa varðandi rest

--- Tools/CryptoGraphy/Substition/Ceasar.cs
+++ Tools/CryptoGraphy/Substition/Ceasar.cs
@@ -1,13 +1,13 @@
-﻿namespace Tools.CryptoGraphy.Substition
+﻿namespace Tools.Cryptography.Substition
 {
     public class Ceasar
     {
-        string Encrypt(string Text)
+        string Encrypt(string Text,int shift)
         {
             return Text;
         }
 
-        string Decrypt(string Text)
+        string Decrypt(string Text,int shift)
         {
             return Text;
         }
