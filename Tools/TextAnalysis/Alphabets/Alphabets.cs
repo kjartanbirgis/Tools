5266737391d646ead1098bc777f4fb1a90e28420
Kjartan Birgisson <kjartan@stubbur.com>
2026-01-18 16:35:38 +0000

    Breytti úr string í char og tók út z og bætti við nýju strófu fyrir 1974

--- Tools/TextAnalysis/Alphabets/Alphabets.cs
+++ Tools/TextAnalysis/Alphabets/Alphabets.cs
@@ -8,15 +8,20 @@ namespace Tools.TextAnalysis.Language
 {
     internal static class Alphabets
     {
-        static readonly string[] IcelandicAlphabet = new string[]
+        static readonly char[] IcelandicAlphabetPre1974 = new char[]
+{
+            'a','á','b','d','ð','e','é','f','g','h','i','í','j','k','l','m',
+            'n','o','ó','p','r','s','t','u','ú','v','x','y','ý','z','þ','æ','ö'
+};
+        static readonly char[] IcelandicAlphabet = new char[]
         {
-            "a","á","b","d","ð","e","é","f","g","h","i","í","j","k","l","m",
-            "n","o","ó","p","r","s","t","u","ú","v","x","y","ý","z","þ","æ","ö"
+            'a','á','b','d','ð','e','é','f','g','h','i','í','j','k','l','m',
+            'n','o','ó','p','r','s','t','u','ú','v','x','y','ý','þ','æ','ö'
         };
-        static readonly string[] IcelandicAlphabetWithEnglishLetters = new string[]
+        static readonly char[] IcelandicAlphabetWithEnglishLetters = new char[]
         {
-            "a","á","b","c","d","ð","e","é","f","g","h","i","í","j","k","l","m",
-            "n","o","ó","p","q","r","s","t","u","ú","v","w","x","y","ý","z","þ","æ","ö"
+            'a','á','b','c','d','ð','e','é','f','g','h','i','í','j','k','l','m',
+            'n','o','ó','p','q','r','s','t','u','ú','v','w','x','y','ý','z','þ','æ','ö'
         };
     }
 }
