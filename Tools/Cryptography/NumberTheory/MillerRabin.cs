a07eec3194021d829b6870a84c4bfa463c59959c
Kjartan Birgisson <kjartan@stubbur.com>
2026-02-01 17:22:11 +0000

    Random Fall milli 2 talna

--- Tools/Cryptography/NumberTheory/MillerRabin.cs
+++ Tools/Cryptography/NumberTheory/MillerRabin.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Numerics;
+using System.Security.Cryptography;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -35,10 +36,10 @@ namespace Tools.Cryptography.NumberTheory
                 d /= 2;
                 s += 1;
             }
-            Random rand = new Random();
+            
             for (int i = 0; i < rounds; i++)
             {
-                BigInteger a = RandomBigInteger(2, n - 2, rand);
+                BigInteger a = RandomBetween(2, n - 2);
                 BigInteger x = BigInteger.ModPow(a, d, n);
                 if (x == 1 || x == n - 1)
                     continue;
@@ -57,6 +58,30 @@ namespace Tools.Cryptography.NumberTheory
                 return false;
             }
             return true;
+        }
+        
+        /// <summary>
+        /// Generates a cryptographically secure random BigInteger within the specified inclusive range.
+        /// </summary>
+        /// <param name="minInclusive">The inclusive lower bound of the random number to generate.</param>
+        /// <param name="maxInclusive">The inclusive upper bound of the random number to generate.</param>
+        /// <returns>A random BigInteger greater than or equal to minInclusive and less than or equal to maxInclusive.</returns>
+        /// <exception cref="ArgumentException">Thrown when minInclusive is greater than maxInclusive.</exception>
+        
+        private static BigInteger RandomBetween(BigInteger minInclusive, BigInteger maxInclusive)
+        {
+            if (minInclusive > maxInclusive) throw new ArgumentException("min > max");
+
+            BigInteger range = maxInclusive - minInclusive + 1;
+            int bytes = range.GetByteCount(isUnsigned: true);
+
+            while (true)
+            {
+                byte[] buf = RandomNumberGenerator.GetBytes(bytes);
+                BigInteger x = new BigInteger(buf, isUnsigned: true, isBigEndian: false);
+                if (x < range)
+                    return minInclusive + x;
+            }
         }
     }
 }
